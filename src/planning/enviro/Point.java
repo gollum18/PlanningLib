ab07ffd88a61992a16f23667793a8360685dfd65
Christen Ford <christen0ford@gmail.com>
2018-08-24 16:23:01 -0400

    Update ignore file

--- src/planning/enviro/Point.java
+++ src/planning/enviro/Point.java
@@ -2,9 +2,72 @@ package planning.enviro;
 
 public class Point {
 
-    private int iX;
-    private int iY;
+    /*
+        Fields
+     */
 
+    private int iX;     // The x ordinal for the point.
+    private int iY;     // The y ordinal for the point.
+
+    /*
+        Constructors
+     */
+
+    /**
+     * Creates an instance of a Point object.
+     * @param x int: The x ordinal of the point.
+     * @param y int: The y ordinal of the point.
+     */
+    public Point(int x, int y) {
+        iX = x;
+        iY = y;
+    }
+
+    /*
+        Accessors/Mutators
+     */
+
+    /**
+     * Gets the x ordinal of the point.
+     * @return int: The x ordinal of the point.
+     */
+    public int getX() {
+        return iX;
+    }
+
+    /**
+     * Updates the x ordinal of the point.
+     * @param x int: A x ordinal for the point.
+     */
+    public void setX(int x) {
+        iX = x;
+    }
+
+    /**
+     * Gets the y ordinal of the point.
+     * @return int: The y ordinal of the point.
+     */
+    public int getY() {
+        return iY;
+    }
+
+    /**
+     * Updates the y ordinal of the point.
+     * @param y int: A y ordinal for the point.
+     */
+    public void setY(int y) {
+        iY = y;
+    }
+
+    /*
+        Methods
+     */
+
+    /**
+     * Determines if an object is equal to this point.
+     * @param object The object to check.
+     * @return boolean: Whether the object is equal to the point.
+     */
     @Override
     public boolean equals(Object object) {
         if (object == null) {
@@ -17,6 +80,10 @@ public class Point {
         return iX == p.getX() && iY == p.getY();
     }
 
+    /**
+     * Generates a hascode for the point.
+     * @return int: A hashcode for the point.
+     */
     @Override
     public int hashCode() {
         int result = iX;
@@ -24,30 +91,13 @@ public class Point {
         return result;
     }
 
+    /**
+     * Generates a string representation for thr point.
+     * @return string: The string representation of the point.
+     */
     @Override
     public String toString() {
         return String.format("Coordinates: (%d, %d)", iX, iY);
-    }
-
-    public Point(int x, int y) {
-        iX = x;
-        iY = y;
-    }
-
-    public int getX() {
-        return iX;
-    }
-
-    public void setX(int x) {
-        iX = x;
-    }
-
-    public int getY() {
-        return iY;
-    }
-
-    public void setY(int y) {
-        iY = y;
     }
 
 }
